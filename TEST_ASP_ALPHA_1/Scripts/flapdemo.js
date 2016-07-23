d5e1dde6dbb1b29fbda9ce47fdf52571ba8682ba
Prabash <prabash.darshana@gmail.com>
2016-07-23 22:45:26 +0530

    changed flapCounter length to 7 changed timeoutInterval to refresh to 3000

--- TEST_ASP_ALPHA_1/Scripts/flapdemo.js
+++ TEST_ASP_ALPHA_1/Scripts/flapdemo.js
@@ -55,7 +55,7 @@ var FlapDemo = function (display_selector, input_selector, click_selector) {
     this.opts = {
         chars_preset: 'alphanum',
         align: 'left',
-        width: 5,
+        width: 7,
         on_anim_start: onAnimStart,
         on_anim_end: onAnimEnd
     };
